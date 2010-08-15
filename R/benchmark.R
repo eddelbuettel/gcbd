69eaddb59bc1f330dcddd307f9b2034759d32dde
Dirk Eddelbuettel <edd@debian.org>
2010-08-15 18:14:13 +0000

    added svnBenchmark and GPU variant

--- R/benchmark.R
+++ R/benchmark.R
@@ -29,3 +29,15 @@ qrBenchmarkgputools <- function(N, n, trim=0.1) {
     traw <- replicate(n, system.time(gpuQr(a))["elapsed"])
     tmean <- mean(traw,trim=trim)
 }
+
+svdBenchmark <- function(N, n, trim=0.1) {
+    a <- getMatrix(N)
+    traw <- replicate(n, system.time(svd(a))["elapsed"])
+    tmean <- mean(traw,trim=trim)
+}
+
+svdBenchmarkgputools <- function(N, n, trim=0.1) {
+    a <- getMatrix(N)
+    traw <- replicate(n, system.time(gpuSvd(a))["elapsed"])
+    tmean <- mean(traw,trim=trim)
+}

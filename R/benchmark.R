d62641caa8fdbcd3681b124764acfa7cf92bc1e4
Dirk Eddelbuettel <edd@debian.org>
2010-08-22 01:00:03 +0000

    back to using elapsed only

--- R/benchmark.R
+++ R/benchmark.R
@@ -14,67 +14,74 @@ getMagmaMatrix <- function(N) {
 
 matmultBenchmark <- function(N, n, trim=0.1) {
     a <- getMatrix(N)
-    traw <- replicate(n, sum(system.time(crossprod(a))[-3]))
+    traw <- replicate(n, system.time(crossprod(a))[3])
     tmean <- mean(traw,trim=trim)
 }
 
 matmultBenchmarkmagma <- function(N, n, trim=0.1) {
     a <- getMagmaMatrix(N)
-    traw <- replicate(n, sum(system.time(crossprod(a))[-3]))
+    traw <- replicate(n, system.time(crossprod(a))[3])
     tmean <- mean(traw,trim=trim)
 }
 
 matmultBenchmarkgputools <- function(N, n, trim=0.1) {
     a <- getMatrix(N)
-    traw <- replicate(n, sum(system.time(gpuMatMult(a,a))[-3]))
+    traw <- replicate(n, system.time(gpuMatMult(a,a))[3])
     tmean <- mean(traw,trim=trim)
 }
 
 qrBenchmark <- function(N, n, trim=0.1) {
     a <- getMatrix(N)
-    traw <- replicate(n, sum(system.time(qr(a))[-3]))
+    traw <- replicate(n, system.time(qr(a))[3])
     tmean <- mean(traw,trim=trim)
 }
 
 qrBenchmarkmagma <- function(N, n, trim=0.1) {
     a <- getMagmaMatrix(N)
-    traw <- replicate(n, sum(system.time(qr(a))[-3]))
+    traw <- replicate(n, system.time(qr(a))[3])
     tmean <- mean(traw,trim=trim)
 }
 
 qrBenchmarkgputools <- function(N, n, trim=0.1) {
     a <- getMatrix(N)
-    traw <- replicate(n, sum(system.time(gpuQr(a))[-3]))
+    traw <- replicate(n, system.time(gpuQr(a))[3])
     tmean <- mean(traw,trim=trim)
 }
 
 svdBenchmark <- function(N, n, trim=0.1) {
     a <- getMatrix(N)
-    traw <- replicate(n, sum(system.time(svd(a))[-3]))
+    traw <- replicate(n, system.time(svd(a))[3])
     tmean <- mean(traw,trim=trim)
 }
 
+#svdBenchmarkTEST <- function(N, n, trim=0.1) {
+#    a <- getMatrix(N)
+#    traw1 <- replicate(n, system.time(svd(a))); print(traw1)
+#    traw <- replicate(n, system.time(svd(a))[3])
+#    tmean <- mean(traw,trim=trim)
+#}
+
 svdBenchmarkmagma <- function(N, n, trim=0.1) {
     a <- getMagmaMatrix(N)
-    traw <- replicate(n, sum(system.time(svd(a))[-3]))
+    traw <- replicate(n, system.time(svd(a))[3])
     tmean <- mean(traw,trim=trim)
 }
 
 svdBenchmarkgputools <- function(N, n, trim=0.1) {
     a <- getMatrix(N)
-    traw <- replicate(n, sum(system.time(gpuSvd(a))[-3]))
+    traw <- replicate(n, system.time(gpuSvd(a))[3])
     tmean <- mean(traw,trim=trim)
 }
 
 luBenchmark <- function(N, n, trim=0.1) {
     a <- getMatrix(N)
-    traw <- replicate(n, sum(system.time(lu(a))[-3]))
+    traw <- replicate(n, system.time(lu(a))[3])
     tmean <- mean(traw,trim=trim)
 }
 
 luBenchmarkmagma <- function(N, n, trim=0.1) {
     a <- getMagmaMatrix(N)
-    traw <- replicate(n, sum(system.time(lu(a))[-3]))
+    traw <- replicate(n, system.time(lu(a))[3])
     tmean <- mean(traw,trim=trim)
 }
 

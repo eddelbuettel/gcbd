0ddb4a94ddc7953c6520e5b3f8383f014d8c5d8a
Dirk Eddelbuettel <edd@debian.org>
2010-09-01 16:38:33 +0000

    updated

--- inst/scripts/runMe.sh
+++ inst/scripts/runMe.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-for n in 250 500 750 1000 1250 1500 1750 2000 2250 2500 2750 3000 3250 3500 3750 4000; 
-#for n in 250 500 750 1000 1250 1500 1750 2000 2250 2500; 
-#for n in 2250 2500 3000 3250 3500 3750 4000; 
-#for n in 250;
-do
-    if [ $n -gt 2000 ]
-    then
-    	r=10
-    else
-        r=20
-    fi
-    echo "Now starting $n $r"
-    ./benchmark.r -n $n -r $r -b lu -v
+#for b in lu qr svd matmult;
+for b in qr svd matmult;
+do 
+    ##for n in 250 500 750 1000 1250 1500 1750 2000 2250 2500 2750 3000 3250 3500 3750 4000; 
+	for n in 100 200 300 400 500 600 700 800 900 1000 1250 1500 1750 2000 2500 3000 3500 4000 4500 5000;
+	do
+		if [ $n -lt 1000 ]; then
+    		r=50
+		elif [ $n -lt 2000 ]; then
+			r=30
+		elif [ $n -lt 3000 ]; then
+			r=20
+		elif [ $n -lt 4000 ]; then
+			r=5
+		fi
+		echo "Now starting $n $r $b"
+		./benchmark.r -n $n -r $r -b lu -v
+	done
 done
